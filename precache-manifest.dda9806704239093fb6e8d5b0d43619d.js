318f5c440d10f721141330b4a0edbada914e5710
Yorick <772479358@qq.com>
2019-04-01 12:01:44 -0400

    Updates

---
+++ precache-manifest.dda9806704239093fb6e8d5b0d43619d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de93c7de815c0bf4cb79",
+    "url": "/fire-cook/static/css/main.5283eb6d.chunk.css"
+  },
+  {
+    "revision": "de93c7de815c0bf4cb79",
+    "url": "/fire-cook/static/js/main.5722b681.chunk.js"
+  },
+  {
+    "revision": "585eb6865626f8a7e6f4",
+    "url": "/fire-cook/static/js/runtime~main.2d904e75.js"
+  },
+  {
+    "revision": "0a6d7cf46a3c4b6e3db6",
+    "url": "/fire-cook/static/js/2.967674fd.chunk.js"
+  },
+  {
+    "revision": "d8b527e20b1128db1a8d29969b3a72c6",
+    "url": "/fire-cook/index.html"
+  }
+];
